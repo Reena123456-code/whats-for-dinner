79a064ca14f36d73734f210736d5cb4c3c86dff6
Reena123456-code <reenaorourke@gmail.com>
2023-10-12 23:18:05 +0000

    Add buttons to recipes and start javascript

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("buttons");
+
+    for(let button of buttons) {
+        button.addEventListener()
+    }
+})
+
+function runRecipe() {
+
+}
+
+function checkIngredients() {
+
+}
+
+function addShopping() {
+
+}
+
+function addStore() {
+    
+}
