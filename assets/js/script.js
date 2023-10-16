64f8a2ac74b78aee6762cc590e53a26cb699b2b1
Reena123456-code <reenaorourke@gmail.com>
2023-10-16 10:56:32 +0000

    Add recipes

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,73 +23,112 @@ document.addEventListener("DOMContentLoaded", function() {
 
         let value = 0;
 
-        function 
-
-document.getElementsByClassName("pantry-container").innerHtml; */
+        function*/
 
 function runPantry() {
+    document.getElementsByClassName("pantry-container").innerHTML
+    alert("pantry-container");
     let runPantry = {
-        aubergine: 1,
-        babySpinachLeaves: 90,
-        bayLeaves: 3,
-        beefStock: 4,
-        brandy: 35.5,
-        butter: 454,
-        unsaltedButter: 227,
-        carrots: 5,
-        cherryTomatoes: 250 * 2,
-        chickenStock: 4,
-        chives: 20,
-        cream: 250,
-        doubleCream: 250,
-        driedDill: 7,
-        eggs: 6,
-        extraVirginOliveOil: 500,
-        fishStock: 8,
-        frozenPeas: 1000,
-        garlic: 6,
-        hakeFillets: 250 * 2,
-        tomatoKetchup: 460,
-        lambMince: 533,
-        beefMince: 554,
-        lambKidneys: 225,
-        largeSkinlessChickenBreast: 3,
-        lemon: 1,
-        milk: 1000,
-        mushrooms: 300,
-        oliveOil: 1000,
-        onion: 6,
-        paprika: 52,
-        cayennePepper: 48,
-        parsley: 11,
-        plainFlour: 1000,
-        potatoes: 2000,
-        puffPastry: 320,
-        redPepper: 1,
-        salmon: 260,
-        salt: 750,
-        seaSalt: 360,
-        blackPeppercorns: 50,
-        shallots: 300,
-        smokedFish: 400,
-        sourCream: 300,
-        sprigOfThyme: 25,
-        steak: 700,
-        tagliatelle: 500,
-        tomatoPuree: 200,
-        vegetableOil: 1000,
-        whiteFishFillets: 500,
-        worcestershireSauce: 290,
-    }
+     }
 }
 
 function cookRecipe() {
-    let cook() = (runPantry - cookRecipe());
+
 }
 
+function checkRecipe() {
+    document.getElementsByClassName("recipe1-container").innerHTML
+      let recipe1 = {
+        vegetableOil: 15,
+        butter: 5,
+        potatoes: 4,
+        carrots: 2,
+        onion: 1,
+        fishStock: 1,
+        driedDill: 6.28,
+        smlHakeFillets: 4,
+        salmonFillets: 2,
+        smokedFish: 400,
+        cream: 250,
+        milk: 100,
+        chives: 4,
+    }
 
+    let recipe2 = {
+        lambKidneys: 225,
+        steak: 700,
+        vegetableOil: 15,
+        butter: 30,
+        onion: 2,
+        plainFlour: 30,
+        bayLeaves: 2,
+        sprigsOfThyme: 4,
+        beefStockCube: 1,
+        mushrooms: 100,
+        tomatoPuree: 15,
+        worcestershireSauce: 15,
+        parsley: 11.52,
+        salt: 0.5,
+        blackPeppercorns: 0.5,
+        puffPastry: 320,
+    }
 
-function checkRecipe() {}
+    let recipe3 = {
+        onions: 3,
+        carrots: 2,
+        oliveOil: 30,
+        butter: 50,
+        sprigOfThyme: 1,
+        mincedLambOrBeef: 500,
+        tomatoPuree: 15,
+        worcestershireSauce: 30,
+        beefOrChickenStockCube: 1,
+        frozenPeas: 250,
+        potatoes: 500,
+        milk: 250,
+        butter: 100,
+        eggYolks: 2,
+    }
+    
+    let recipe4 = {
+        filletOfBeef: 600,
+        plainFlour: 30,
+        paprikaOrCayennePepper: 0.5,
+        mushrooms: 225,
+        sourCream: 250,
+        brandy: 35.5,
+        shallots: 2,
+        garlic: 1,
+    }
+
+    let recipe5 = {
+        cherryTomatoes: 500,
+        aubergine: 1,
+        redPepper: 1,
+        garlic: 4,
+        oliveOil: 100,
+        sprigsOfThyme: 4,
+        whiteFishFillets: 500,
+        extraVirginOliveOil: 15,
+        crustyBread: 1,
+        greenLeaves: 1,
+    }
+    let recipe6 = {
+        tagliatelle: 300,
+        oliveOil: 15,
+        unsaltedButter: 30,
+        largeSkinlessChickenBreasts: 2,
+        garlic: 3,
+        doubleCream: 250,
+        frozenPeas: 100,
+        zest1Lemon: 1,
+        semiDriedCherryTomatoes: 50,
+        babySpinachLeaves: 150,
+        seaSalt: 0.5,
+        blackPeppercorns: 0.5,
+    }
+      }
+
 
 function addShopping() {
 
